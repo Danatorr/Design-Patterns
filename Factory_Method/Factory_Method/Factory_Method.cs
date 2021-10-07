4e7291c6288df1430699ac6aa86a89178c2c86f4
Torres <dan03torres@gmail.com>
2021-10-07 16:23:36 -0300

    Initial commit

---
+++ Factory_Method/Factory_Method/Factory_Method.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Design_Patterns
+{
+    class Factory_Method
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
